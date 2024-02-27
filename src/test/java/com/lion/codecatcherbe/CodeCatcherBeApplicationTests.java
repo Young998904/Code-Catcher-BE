9d09964c30d156baef736a8a28df3d972357d50c
Cho Ah Young <like0998904@gmail.com>
2024-02-27 12:45:06 +0900

    init project

---
+++ src/test/java/com/lion/codecatcherbe/CodeCatcherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lion.codecatcherbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCatcherBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
