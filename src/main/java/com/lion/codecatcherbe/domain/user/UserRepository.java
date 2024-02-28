e707a81cfa66939188525229aec630049b12283a
Cho Ah Young <like0998904@gmail.com>
2024-02-28 17:11:53 +0900

    Add #4 - UserRepository 생성

---
+++ src/main/java/com/lion/codecatcherbe/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package com.lion.codecatcherbe.domain.user;
+
+import com.lion.codecatcherbe.domain.user.model.User;
+import java.util.Optional;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
