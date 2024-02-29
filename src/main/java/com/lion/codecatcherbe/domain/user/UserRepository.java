eabb5f81139804be85462c1bb61bfa0a3470a99f
Cho Ah Young <like0998904@gmail.com>
2024-02-29 11:36:04 +0900

    Refactor #4 - @Repository 어노테이션 삭제

--- src/main/java/com/lion/codecatcherbe/domain/user/UserRepository.java
+++ src/main/java/com/lion/codecatcherbe/domain/user/UserRepository.java
@@ -3,9 +3,7 @@ package com.lion.codecatcherbe.domain.user;
 import com.lion.codecatcherbe.domain.user.model.User;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
