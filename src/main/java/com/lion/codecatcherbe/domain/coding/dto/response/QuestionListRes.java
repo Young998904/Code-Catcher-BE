f82fc33099ad2e0d46ce8f10b77ba9200ea45634
Cho Ah Young <like0998904@gmail.com>
2024-03-08 10:57:06 +0900

    Add master - GET /coding/questionlist API 에 대한 QuestionListRes DTO 생성

---
+++ src/main/java/com/lion/codecatcherbe/domain/coding/dto/response/QuestionListRes.java
@@ -0,0 +1,23 @@
+package com.lion.codecatcherbe.domain.coding.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class QuestionListRes {
+
+    QuestionInfo question_1;
+    QuestionInfo question_2;
+    QuestionInfo question_3;
+    @AllArgsConstructor
+    @Getter
+    public static class QuestionInfo {
+        private Long id;
+        private Long level;
+        private String title;
+        private String subject;
+        private String script;
+    }
+}
