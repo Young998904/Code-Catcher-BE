634f39f8b14d2f3ce4c34629b75c299681525a42
Cho Ah Young <like0998904@gmail.com>
2024-03-13 11:56:31 +0900

    Add 코딩테스트/issue25 - userId 와 problemId 를 조건으로 isSuccess 값만 가지고 오는 쿼리문 함수 생성

--- src/main/java/com/lion/codecatcherbe/domain/score/repository/SubmitRepository.java
+++ src/main/java/com/lion/codecatcherbe/domain/score/repository/SubmitRepository.java
@@ -3,8 +3,12 @@ package com.lion.codecatcherbe.domain.score.repository;
 import com.lion.codecatcherbe.domain.score.model.Submit;
 import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface SubmitRepository extends MongoRepository<Submit, String> {
 
     Optional<Submit> findByUserIdAndProblemId(String userId, Long problemId);
+
+    @Query(value="{ 'userId' : ?0, 'problemId' : ?1 }", fields="{ 'isSuccess' : 1, '_id': 0 }")
+    Optional<Submit> findIsSuccessByUserIdAndProblemId(String userId, Long problemId);
 }
