e801bea882c973de6d14c663fd5bb0842d18f764
Cho Ah Young <like0998904@gmail.com>
2024-03-13 11:57:24 +0900

    Add 코딩테스트/issue25 - (오늘의 3문제 가져오기) GET /coding/questionlist 실제 API 구현 - 기존에 구현했던 API 는 유저의 풀이 성공 여부가 포함되어있지 않아 포함되어있는 형태로 재구현 진행

--- src/main/java/com/lion/codecatcherbe/domain/coding/CodingController.java
+++ src/main/java/com/lion/codecatcherbe/domain/coding/CodingController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -75,8 +76,8 @@ public class CodingController {
     }
 
     @GetMapping("/questionlist")
-    public ResponseEntity<QuestionListRes> findQuestionList() {
-        return codingService.findProblemList();
+    public ResponseEntity<QuestionListRes> findQuestionList(@RequestHeader(value = "Authorization", required = false) String token) {
+        return codingService.findProblemList(token);
     }
 
     @GetMapping("/mock/gpt/feedback")
--- src/main/java/com/lion/codecatcherbe/domain/coding/service/CodingService.java
+++ src/main/java/com/lion/codecatcherbe/domain/coding/service/CodingService.java
@@ -6,10 +6,17 @@ import com.lion.codecatcherbe.domain.coding.dto.response.QuestionListRes.Questio
 import com.lion.codecatcherbe.domain.coding.dto.response.QuestionRes;
 import com.lion.codecatcherbe.domain.coding.model.Problem;
 import com.lion.codecatcherbe.domain.coding.repository.ProblemRepository;
+import com.lion.codecatcherbe.domain.score.model.Submit;
+import com.lion.codecatcherbe.domain.score.repository.SubmitRepository;
+import com.lion.codecatcherbe.domain.user.UserRepository;
+import com.lion.codecatcherbe.domain.user.model.User;
+import com.lion.codecatcherbe.infra.kakao.security.TokenProvider;
+import java.sql.SQLOutput;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 import java.util.List;
+import javax.swing.text.StyledEditorKit.BoldAction;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -25,7 +32,30 @@ import org.springframework.transaction.annotation.Transactional;
 public class CodingService {
     private final SequenceGeneratorService sequenceGeneratorService;
     private final ProblemRepository problemRepository;
+    private final SubmitRepository submitRepository;
+    private final UserRepository userRepository;
     private final MongoOperations mongoOperations;
+
+    public String filterJwt (String token) {
+        String jwt = null;
+
+        if (token != null && token.startsWith("Bearer ")) {
+            jwt = token.substring(7);
+        }
+
+        // 토큰 사용하여 필요한 로직 처리
+        return jwt;
+    }
+
+    public String getUserId(String jwt) {
+        String userId;
+        try {
+            userId = TokenProvider.getSubject(jwt);
+        } catch (Exception e) {
+            return null;
+        }
+        return userId;
+    }
     @Transactional
     public HttpStatus saveProblem(String content) {
         ObjectMapper mapper = new ObjectMapper();
@@ -71,20 +101,47 @@ public class CodingService {
         return new ResponseEntity<>(questionRes, HttpStatus.OK);
     }
 
-    public ResponseEntity<QuestionListRes> findProblemList() {
+    public ResponseEntity<QuestionListRes> findProblemList(String token) {
+        // 오늘 날짜를 가져오고 끝날짜를 24시간 뒤로 설정하여 문제의 정보를 가지고 옴
         LocalDateTime start = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS).plusHours(9L);
         List<Problem> problemList = findProblemsByCreatedAt(start, start.plusHours(24L));
 
+        // 레벨 순으로 정렬
         problemList.sort(Comparator.comparingLong(Problem::getLevel));
 
         Problem p1 = problemList.get(0);
         Problem p2 = problemList.get(1);
         Problem p3 = problemList.get(2);
 
+        // 유저의 Submit 기록 확인
+        String jwt = filterJwt(token);
+
+        String userId = getUserId(jwt);
+
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        User user = userRepository.findById(userId).orElse(null);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        Submit s1 = submitRepository.findIsSuccessByUserIdAndProblemId(user.getId(), p1.getId()).orElse(null);
+        Submit s2 = submitRepository.findIsSuccessByUserIdAndProblemId(user.getId(), p2.getId()).orElse(null);
+        Submit s3 = submitRepository.findIsSuccessByUserIdAndProblemId(user.getId(), p3.getId()).orElse(null);
+
+        Boolean b1, b2, b3;
+
+        if (s1 == null) b1 = null; else b1 = s1.getIsSuccess();
+        if (s2 == null) b2 = null; else b2 = s2.getIsSuccess();
+        if (s3 == null) b3 = null; else b3 = s3.getIsSuccess();
+
         QuestionListRes questionListRes = QuestionListRes.builder()
-            .question_1(new QuestionInfo(p1.getId(), null, p1.getLevel(), p1.getTitle(), p1.getSubject(), p1.getScript()))
-            .question_2(new QuestionInfo(p2.getId(), null, p2.getLevel(), p2.getTitle(), p2.getSubject(), p2.getScript()))
-            .question_3(new QuestionInfo(p3.getId(), null, p3.getLevel(), p3.getTitle(), p3.getSubject(), p3.getScript()))
+            .question_1(new QuestionInfo(p1.getId(), b1, p1.getLevel(), p1.getTitle(), p1.getSubject(), p1.getScript()))
+            .question_2(new QuestionInfo(p2.getId(), b2, p2.getLevel(), p2.getTitle(), p2.getSubject(), p2.getScript()))
+            .question_3(new QuestionInfo(p3.getId(), b3, p3.getLevel(), p3.getTitle(), p3.getSubject(), p3.getScript()))
             .build();
 
         return new ResponseEntity<>(questionListRes, HttpStatus.OK);
