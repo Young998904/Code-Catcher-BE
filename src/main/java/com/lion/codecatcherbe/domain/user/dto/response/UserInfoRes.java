83567187c493e08a7e3b10a80531815213b1f276
Cho Ah Young <like0998904@gmail.com>
2024-03-14 15:11:18 +0900

    Add 유저/issue36 - GET /user/info 실제 API 구현 - UserInfoRes DTO 추가

---
+++ src/main/java/com/lion/codecatcherbe/domain/user/dto/response/UserInfoRes.java
@@ -0,0 +1,21 @@
+package com.lion.codecatcherbe.domain.user.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfoRes {
+    private Long userId;
+    private String nickname;
+    private String email;
+    private int level;
+    private int exp;
+    private int totalCnt;
+    private int completeCnt;
+    private int bookmarkCnt;
+}
