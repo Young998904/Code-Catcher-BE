4b505adadd07ea1ed695579237e4bd6364b7a4c2
Cho Ah Young <like0998904@gmail.com>
2024-03-18 16:59:01 +0900

    Add 북마크/issue32 - (북마크 다중 삭제) DELETE /bookmark/many 와 (북마크 단일 삭제) DELETE /bookmark/one?id 실제 API 구현 - (북마크 저장) POST /bookmark 리턴값을 저장한 Bookmark id 값으로 수정 - 전달 받은 삭제 id 값을 바탕으로 Bookmark 객체 리스틀 받아오는 findByIdIn() 함수 BookmarkRepository 에 추가

--- src/main/java/com/lion/codecatcherbe/domain/bookmark/BookmarkController.java
+++ src/main/java/com/lion/codecatcherbe/domain/bookmark/BookmarkController.java
@@ -1,10 +1,13 @@
 package com.lion.codecatcherbe.domain.bookmark;
 
+import com.lion.codecatcherbe.domain.bookmark.dto.BookMarkDeleteRes;
 import com.lion.codecatcherbe.domain.bookmark.dto.BookMarkInfoRes;
 import com.lion.codecatcherbe.domain.bookmark.dto.BookMarkReq;
+import com.lion.codecatcherbe.domain.mypage.dto.BookmarkMoreInfoRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
     @PostMapping()
-    public HttpStatus saveBookmark(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody BookMarkReq bookMarkReq) {
+    public ResponseEntity<String> saveBookmark(@RequestHeader(value = "Authorization", required = false) String token, @RequestBody BookMarkReq bookMarkReq) {
         return bookmarkService.saveBookmark(token, bookMarkReq);
     }
 
     @GetMapping()
     public ResponseEntity<BookMarkInfoRes> findBookmark(@RequestHeader(value = "Authorization", required = false) String token, @RequestParam (name = "id") String bookmarkId) {
         return bookmarkService.findBookmarkInfo(token, bookmarkId);
+    }
+
+    @DeleteMapping("/one")
+    public HttpStatus deleteOneBookmark (@RequestHeader(value = "Authorization", required = false) String token, @RequestParam (name = "id") String bookmarkId) {
+        return bookmarkService.deleteOneBookmark(token, bookmarkId);
+    }
+
+    @DeleteMapping("/many")
+    public ResponseEntity<BookmarkMoreInfoRes> deleteManyBookmark (@RequestHeader(value = "Authorization", required = false) String token, @RequestBody BookMarkDeleteRes bookMarkDeleteRes) {
+        return bookmarkService.deleteManyBookmark(token, bookMarkDeleteRes);
     }
 }
