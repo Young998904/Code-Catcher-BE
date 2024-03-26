116c81c6690381cda3fd5f0acffe8be9b71005ba
Cho Ah Young <like0998904@gmail.com>
2024-03-26 18:22:30 +0900

    Add GPT/issue49 - POST /gpt/mock/feedback MockAPI 구현

--- src/main/java/com/lion/codecatcherbe/infra/gpt/dto/response/GPTReviewRes.java
+++ src/main/java/com/lion/codecatcherbe/infra/gpt/dto/response/GPTReviewRes.java
@@ -1,5 +1,7 @@
 package com.lion.codecatcherbe.infra.gpt.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class GPTReviewRes {
     private String time;
     private String memory;
