52b476ff1d50d497d5d58d3176ed049db7725678
Cho Ah Young <like0998904@gmail.com>
2024-03-26 11:56:03 +0900

    Add GPT/issue49 - (GPT 문제 자동 생성) GET /gpt?subject&level 구현 - 파라미터로 주제와 레벨을 입력하면 문제 생성 후 자동으로 DB 저장

--- src/main/java/com/lion/codecatcherbe/infra/gpt/GPTController.java
+++ src/main/java/com/lion/codecatcherbe/infra/gpt/GPTController.java
@@ -3,10 +3,13 @@ package com.lion.codecatcherbe.infra.gpt;
 import com.lion.codecatcherbe.infra.gpt.dto.request.CodeReviewReq;
 import com.lion.codecatcherbe.infra.gpt.dto.response.GPTReviewRes;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class GPTController {
 
     private final GPTService gptService;
+    /*
+        ChatGPT 문제 생성
+    */
+    @GetMapping("")
+    public HttpStatus gptGenTest (@RequestParam String subject, @RequestParam Long level) {
+        return gptService.getGptProblem(subject, level);
+    }
 
     /*
         ChatGPT 내 코드 피드백
