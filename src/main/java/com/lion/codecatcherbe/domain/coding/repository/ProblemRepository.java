83950e9a716d07238c51ec05ce604bb4d8fa9ee2
Cho Ah Young <like0998904@gmail.com>
2024-03-27 20:51:15 +0900

    Add master - ProblemRepository 에 findByCreatedAtBetween() 추가

--- src/main/java/com/lion/codecatcherbe/domain/coding/repository/ProblemRepository.java
+++ src/main/java/com/lion/codecatcherbe/domain/coding/repository/ProblemRepository.java
@@ -1,8 +1,11 @@
 package com.lion.codecatcherbe.domain.coding.repository;
 
 import com.lion.codecatcherbe.domain.coding.model.Problem;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ProblemRepository extends MongoRepository<Problem, Long> {
 
+    List<Problem> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 }
