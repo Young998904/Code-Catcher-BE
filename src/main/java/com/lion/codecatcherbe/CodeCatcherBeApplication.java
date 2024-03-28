fedd034386c0a4690983f96d178ae9d591ec510b
Cho Ah Young <like0998904@gmail.com>
2024-03-28 16:15:08 +0900

    (#66) Add 유저/issue66 - 내 코드에 대한 GPT 리뷰를 하루에 한번만 실행 가능하도록 - @EnableScheduling 추가

--- src/main/java/com/lion/codecatcherbe/CodeCatcherBeApplication.java
+++ src/main/java/com/lion/codecatcherbe/CodeCatcherBeApplication.java
@@ -2,8 +2,10 @@ package com.lion.codecatcherbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CodeCatcherBeApplication {
 
     public static void main(String[] args) {
