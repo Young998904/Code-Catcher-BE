22f95b69edbe547a3d1a2867df337d4e948e2d12
Cho Ah Young <like0998904@gmail.com>
2024-05-02 20:43:26 +0900

    (#83) Test Refactor/issue83 : 영문판 도메인 추가 - host 추출 방식을 헤더의 X-Forwarded-Host 값으로 변경

--- src/main/java/com/lion/codecatcherbe/infra/kakao/KakaoController.java
+++ src/main/java/com/lion/codecatcherbe/infra/kakao/KakaoController.java
@@ -19,7 +19,7 @@ public class KakaoController {
 
     @RequestMapping("/callback")
     public ResponseEntity<SuccessLoginInfo> kakaoLogin (HttpServletRequest request, @RequestParam String code) throws JsonProcessingException {
-        String host = request.getHeader("Host");
+        String host = request.getHeader("X-Forwarded-Host");
         SuccessLoginInfo info = kakaoService.kakaoLogin(code, host);
         return new ResponseEntity<>(info, generateHeader(info.getJwt()), HttpStatus.OK);
     }
