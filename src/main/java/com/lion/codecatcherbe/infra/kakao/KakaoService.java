c25a9a16d70e42d966d3f042a1ec87599bb56b2a
Cho Ah Young <like0998904@gmail.com>
2024-05-02 19:45:58 +0900

    (#83) Test Refactor/issue83 : 영문판 도메인 추가 - 카카오 로그인 시 redirect_uri 에 https:// 추가

--- src/main/java/com/lion/codecatcherbe/infra/kakao/KakaoService.java
+++ src/main/java/com/lion/codecatcherbe/infra/kakao/KakaoService.java
@@ -75,7 +75,7 @@ public class KakaoService {
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
         body.add("client_id", CLIENT_ID);
-        body.add("redirect_uri", host + "/kakao/callback");
+        body.add("redirect_uri", "https://" + host + "/kakao/callback");
         body.add("code", code);
 
         // HTTP 요청 보내기
