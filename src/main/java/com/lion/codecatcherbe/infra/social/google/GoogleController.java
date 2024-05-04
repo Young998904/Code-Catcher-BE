2e1272dbd7104827b3e2d1c2edfc912cd02b2ce3
Cho Ah Young <like0998904@gmail.com>
2024-05-04 14:47:05 +0900

    (#88) Refactor 유저/issue88 : 구글 소셜 로그인 도입 - 프론트 동작 테스트를 위한 log 추가

--- src/main/java/com/lion/codecatcherbe/infra/social/google/GoogleController.java
+++ src/main/java/com/lion/codecatcherbe/infra/social/google/GoogleController.java
@@ -3,6 +3,7 @@ package com.lion.codecatcherbe.infra.social.google;
 import com.lion.codecatcherbe.infra.social.dto.SuccessLoginInfo;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/google")
 @RequiredArgsConstructor
+@Slf4j
 public class GoogleController {
     private final GoogleService googleService;
 
     @GetMapping("/callback")
-    public  ResponseEntity<SuccessLoginInfo> successGoogleLogin(HttpServletRequest request, @RequestParam("code") String accessCode) {
+    public  ResponseEntity<SuccessLoginInfo> successGoogleLogin(HttpServletRequest request,  @RequestParam String code) {
         String host = request.getHeader("X-Forwarded-Host");
-        SuccessLoginInfo info = googleService.googleLogin(accessCode, host);
+        log.info("host : {}", host);
+        log.info("code : {}", code);
+        SuccessLoginInfo info = googleService.googleLogin(code, host);
         return new ResponseEntity<>(info, generateHeader(info.getJwt()), HttpStatus.OK);
     }
 
